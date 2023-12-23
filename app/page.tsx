d6d660a476d67a2b2e45ebc38faf76b5c316bc3a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-22 21:17:05 -0800

    added notes

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,16 @@ import postgres from "postgres";
 const sql = postgres(process.env.DATABASE_URL!);
 
 // create a quiz type to represent the data from the database
+// quiz_id: This property is of type number. It likely represents a 
+// unique identifier for each quiz.
+// title: This property is of type string. It likely represents the 
+// title or name of the quiz.
 type Quiz = {
   quiz_id: number;
   title: string;
 };
 
+// Fetches data from a database and renders a list of quizzes.
 async function Quizzes() {
   // get all the quizzes from the database
   const quizzes: Quiz[] = await sql`SELECT * FROM quizzes`;
@@ -24,12 +29,12 @@ async function Quizzes() {
   );
 }
 
-// The Home component will render the list of quizzes
+// The Home component will render the list of quizzes and a form to create new quizzes.
 export default function Home() {
   return (
-    <section className="flex flex-col items-center text-center mt-2 py-5 and mx-auto">
+    <section className="flex flex-col items-center text-center mt-2 py-5">
       <h1 className="text-4xl font-bold">All Quizzes</h1>
-      {/* <Quizzes /> */}
+      <Quizzes />
     </section>
   );
 }
--- app/quiz/[id]/page.tsx
+++ app/quiz/[id]/page.tsx
@@ -2,6 +2,52 @@ import postgres from "postgres";
 
 const sql = postgres(process.env.DATABASE_URL!);
 
+async function Quiz({
+  id,
+  searchParams,
+}: {
+  id: number;
+  searchParams: { show?: string };
+}) {
+  // get the quiz from the database
+  // More information in the README.md file
+  let answers = await sql`
+  SELECT
+    q.quiz_id
+    q.title AS quiz_title,
+    q.description AS quiz_description,
+    q.question_text AS quiz_question,
+    a.answer_id,
+    a.answer_text,
+    a.is_correct
+  FROM quizzes AS q
+  JOIN answers AS a ON q.quiz_id = a.quiz_id
+  WHERE q.quiz_id = ${id}
+  `;
+
+  // SELECT is the keyword used to select data from a database.
+  // FROM is the table we are selecting from. In this case, we are selecting from the quizzes table.
+  // JOIN is joining two tables together. In this case, we are joining the quizzes table with the answers table.
+  // WHERE is a condition that we are using to filter the results. In this case, we are filtering the results to only include the quiz with the id that matches the id in the URL.
+
+  // The result of the query is an array of objects. Each object represents a row in the database. The properties of the object are the columns in the database.
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">{answers[0].quiz_title}</h1>
+      <p>{answers[0].quiz_description}</p>
+      <p>{answers[0].question}</p>
+      <ul>
+        {answers.map((answer) => (
+          <li key={answer.answer_id}>
+            {answer.answer_text}
+            {searchParams.show === "true" && answer.is_correct ? "✅" : "❌"}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function QuizPage({
   params,
   searchParams,
